00f1531e0d71b6d60e6ec53513bce2758baf9314
Zainabraja <zainabraja53@gmail.com>
2022-02-26 18:07:04 +0530

    changes

---
+++ browse/movie.js
@@ -0,0 +1,84 @@
+const hidecard = (genre) => {
+    var clicked = document.getElementById(genre)
+    console.log(genre.classList)
+    genre.classList.add("active")
+}
+
+const showmoviecard = (genre, title) => {
+    var clicked = document.getElementById(genre)
+    if (clicked.classList.contains("active")) {
+        clicked.classList.toggle("active")
+    }
+
+    fetch('films.json')
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            switch (genre) {
+                case "popular":
+                    var mydata = data[0].popular
+                    break;
+                case "drama":
+                    var mydata = data[1].drama
+                    break;
+                case "fantasy":
+                    var mydata = data[2].fantasy
+                    break;
+                case "action":
+                    var mydata = data[3].action
+                    break;
+                case "romance":
+                    var mydata = data[4].romance
+                    break;
+                case "thriller":
+                    var mydata = data[5].thriller
+                    break;
+            }
+            var idx = 0
+            for (var i = 0; i < 11; i++) {
+                if (title == mydata[i].title) {
+                    idx = i
+                }
+            }
+            clicked.innerHTML =
+                `<div class="card" style="background-image:  linear-gradient(to left, rgba(255,153,153,0) 0%,rgba(255,153,153,0) 75%,rgba(0, 0, 0, 1) 100%), url(${mydata[idx].image});">
+                <div class="card-left">
+                    <img src="${mydata[idx].image}" alt="">
+                    <h3 class="card-title">${title}</h3>
+                    <p class="card-desc">${mydata[idx].desc}</p>
+                    <button class="play card-btn" onclick="showPopup()">Play</button>
+                    <div class="popup">
+                    <div class="blocker" onclick="hidePopup()"></div>
+                    <div class="contents">
+                    <video controls >
+                    <source src="Oscar Nominated 3D Animated Shorts Sweet Cocoon - by ESMA  TheCGBros.mp4" type="video/mp4">
+                    Your browser does not support HTML video.
+                    </video>
+                    </div>
+                    </div>                 
+                    </div>
+                    <div class="card-right" >
+                    <span class="cross"><i class="bi bi-x-circle" width="40" height="40" onclick="hidecard(${genre})"></i></span>
+                    </div>
+                    </div>`
+        })
+}
+
+const showPopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.classList.add('open');
+    const bi = document.getElementsByClassName("bi")
+    bi[0].style.display = "none"
+}
+
+const hidePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.classList.remove('open');
+    const video = document.querySelector('video')
+    video.pause()
+    video.currentTime = 0;
+    const bi = document.getElementsByClassName("bi")
+    bi[0].style.display = "flex"
+}
+
